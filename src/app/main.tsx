22e0394e9b663be899606198bfec32b6e0715090
JJHyeok13 <jjinh99@naver.com>
2025-07-09 02:06:08 +0900

    feat: TanStackQuery 사용에 따른 QueryClientProvider 추가

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,19 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../index.css";
 import { BrowserRouter } from "react-router-dom";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
 import { ScrollToTop } from "@/shared/ui/scrollToTop";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <GoogleOAuthProvider clientId={CLIENT_ID}>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
         <ScrollToTop />
         <App />
-      </GoogleOAuthProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
