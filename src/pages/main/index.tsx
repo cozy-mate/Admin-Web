59f2c95189b8fb936494ab43d8aeedbedaa8dd4b
JJHyeok13 <jjinh99@naver.com>
2025-07-08 22:54:39 +0900

    fix: 메인 페이지 코드 수정

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,26 +1,18 @@
-import { useNavigate } from "react-router-dom";
-import { GoogleLogin } from "@react-oauth/google";
-
 export default function MainPage() {
-  const navigate = useNavigate();
+  const handleLogin = () => {
+    window.location.href = `${import.meta.env.VITE_API_URL}/admin/auth/login`;
+  };
 
   return (
     <main className="w-dvw h-dvh flex flex-col justify-center items-center gap-y-[80px] pb-[120px]">
-      <div onClick={() => navigate("/inquiry")}>
+      <div>
         <p className="titleLg textDefault text-center">어서오세요,</p>
         <p className="titleLg textDefault text-center">
           cozymate 관리자 페이지입니다
         </p>
       </div>
 
-      <GoogleLogin
-        onSuccess={(credentialResponse) => {
-          console.log(credentialResponse);
-        }}
-        onError={() => {
-          console.error("Failed Login..");
-        }}
-      />
+      <button onClick={handleLogin}>클릭</button>
     </main>
   );
 }
