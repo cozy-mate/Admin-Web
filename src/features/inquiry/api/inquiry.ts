fa4aca666b4ae44a372737d952160bc8d58e0721
JJHyeok13 <jjinh99@naver.com>
2025-07-08 22:51:40 +0900

    feat: 관리자 문의 관련 API 구현

---
+++ src/features/inquiry/api/inquiry.ts
@@ -0,0 +1,37 @@
+import {
+  GuestGetAxiosInstance,
+  GuestPatchAxiosInstance,
+} from "@/shared/lib/axios/guest.axios.method";
+import type { InquiryAnswer } from "@/shared/types/inquiry";
+
+// 코지메이트 전체 문의 리스트 조회
+export const getInquiryList = async (page: number, size: number) => {
+  const response = await GuestGetAxiosInstance(`/admin/inquiries`, {
+    params: {
+      page,
+      size,
+    },
+  });
+
+  return response.data;
+};
+
+// 코지메이트 특정 문의 조회
+export const getInquiryDetail = async (inquiryId: number) => {
+  const response = await GuestGetAxiosInstance(`/admin/inquires/${inquiryId}`);
+
+  return response.data;
+};
+
+// 답변 완료로 변경
+export const updateInquiryComplete = async (
+  inquiryId: number,
+  data: InquiryAnswer
+) => {
+  const response = await GuestPatchAxiosInstance(
+    `/admin/inquires/${inquiryId}`,
+    data
+  );
+
+  return response.data;
+};
