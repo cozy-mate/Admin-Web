175f0d870e0d1ed5b8f5da2735f57b92a94ca39c
JJHyeok13 <jjinh99@naver.com>
2025-07-08 23:25:57 +0900

    fix: response.result 값으로 set하도록 수정

--- src/pages/inquiry/index.tsx
+++ src/pages/inquiry/index.tsx
@@ -38,7 +38,7 @@ export default function InquiryPage() {
       );
       console.log(response);
 
-      setData(response);
+      setData(response.result);
     };
     getData();
   }, [pagination]);
