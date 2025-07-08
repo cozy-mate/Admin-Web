018cd6240cf13a4ab50e3c214fbe8fd9d13a7f18
JJHyeok13 <jjinh99@naver.com>
2025-07-09 02:06:19 +0900

    feat: Suspense 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,20 +5,23 @@ import InquiryPage from "@/pages/inquiry";
 import InquiryDetailPage from "@/pages/inquiryDetail";
 import ReportPage from "@/pages/report";
 import ReportDetailPage from "@/pages/reportDetail";
+import { Suspense } from "react";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
+    <Suspense fallback={<div>조회 중...</div>}>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
 
-      <Route element={<Layout />}>
-        <Route path="/inquiry" element={<InquiryPage />} />
-        <Route path="/inquiry/:id" element={<InquiryDetailPage />} />
+        <Route element={<Layout />}>
+          <Route path="/inquiry" element={<InquiryPage />} />
+          <Route path="/inquiry/:id" element={<InquiryDetailPage />} />
 
-        <Route path="/report" element={<ReportPage />} />
-        <Route path="/report/:id" element={<ReportDetailPage />} />
-      </Route>
-    </Routes>
+          <Route path="/report" element={<ReportPage />} />
+          <Route path="/report/:id" element={<ReportDetailPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
