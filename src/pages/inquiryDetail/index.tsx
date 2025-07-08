7eca80327282dcb25f8efac688f779f8047ea81d
JJHyeok13 <jjinh99@naver.com>
2025-07-09 02:09:31 +0900

    feat: useSuspenseQuery 연결

--- src/pages/inquiryDetail/index.tsx
+++ src/pages/inquiryDetail/index.tsx
@@ -1,23 +1,27 @@
 import { useState } from "react";
-import { detailData } from "@shared/mocks/inquiryData";
+import { useGetInquiryDetail } from "@/features/inquiry/model/inquiry";
+import { useLocation } from "react-router-dom";
 
 export default function InquiryDetailPage() {
-  const data = detailData;
+  const { pathname } = useLocation();
+  const id = pathname.split("/").filter(Boolean).pop();
+
+  const { data } = useGetInquiryDetail(Number(id));
 
   const [answer, setAnswer] = useState<string>("");
 
   return (
     <main className="flex flex-col">
       <p className="self-start bg-[#F6F6F6] px-[16px] py-[4px] rounded-[6px] textMdSemibold textInfo mb-[8px]">
-        {data.status}
+        {data.result.status}
       </p>
 
       {/* <p className="titleSm textDefault mb-[24px]">{data.}</p> */}
 
-      <p className="textMd textSub mb-[32px]">{data.content}</p>
+      <p className="textMd textSub mb-[32px]">{data.result.content}</p>
 
       <p className="textMd textInfo">
-        {data.nickname}, {data.createdAt}
+        {data.result.nickname}, {data.result.createdAt}
       </p>
 
       <div className="bg-[#EBEBEB] h-[1px] my-[32px]" />
@@ -25,19 +29,19 @@ export default function InquiryDetailPage() {
       <div className="flex flex-col gap-y-[40px]">
         <div className="flex flex-col gap-y-[4px]">
           <p className="textMdBold textSub">답변 요청 이메일 주소</p>
-          <p className="textMd textInfo">{data.replyEmail}</p>
+          <p className="textMd textInfo">{data.result.replyEmail}</p>
         </div>
 
-        {data.status === "답변 완료" ? (
+        {data.result.status === "답변 완료" ? (
           <div className="flex flex-col gap-y-[40px]">
             <div className="flex flex-col gap-y-[4px]">
               <p className="textMdBold textSub">답변 내용</p>
-              <p className="textMd textInfo">{data.replyContent}</p>
+              <p className="textMd textInfo">{data.result.replyContent}</p>
             </div>
 
             <div className="flex flex-col gap-y-[4px]">
               <p className="textMdBold textSub">답변 완료 일자</p>
-              <p className="textMd textInfo">{data.replyAt}</p>
+              <p className="textMd textInfo">{data.result.replyAt}</p>
             </div>
           </div>
         ) : (
