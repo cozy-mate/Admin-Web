092006ab151d01610ebb5cfdb88b59bd39c806be
JJHyeok13 <jjinh99@naver.com>
2025-07-18 20:25:58 +0900

    chore: 테스트를 위한 라우팅 메서드 대신 구글로그인용 주소로 변경

--- src/shared/ui/googleButton.tsx
+++ src/shared/ui/googleButton.tsx
@@ -2,23 +2,23 @@
 
 import GoogleIcon from "@/shared/assets/google.svg";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 export default function GoogleButton() {
-  //   const handleLogin = () => {
-  //     window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/admin/auth/login`;
-  //   };
-
-  const router = useRouter();
-
-  const handleTest = () => {
-    router.push("/inquiry");
+  const handleLogin = () => {
+    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/admin/auth/login`;
   };
+
+  // const router = useRouter();
+
+  // const handleTest = () => {
+  //   router.push("/inquiry");
+  // };
 
   return (
     <button
-      //   onClick={handleLogin}
-      onClick={handleTest}
+      onClick={handleLogin}
+      // onClick={handleTest}
       className="lg:w-[440px] flex flex-row items-center gap-x-[16px] p-[16px] border border-[#BABABA] rounded-[10px] cursor-pointer"
     >
       <Image src={GoogleIcon} alt="구글 아이콘" />
