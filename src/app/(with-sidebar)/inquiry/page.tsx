f5223dc21e17176cbd4f4087aa45e95b256cb37e
JJHyeok13 <jjinh99@naver.com>
2025-07-24 17:34:19 +0900

    fix: 쿠키 확인용 코드 추가

--- src/app/(with-sidebar)/inquiry/page.tsx
+++ src/app/(with-sidebar)/inquiry/page.tsx
@@ -5,15 +5,20 @@ import Pagination from "@/shared/ui/pagination";
 import SearchBar from "@/shared/ui/searchBar";
 import { Suspense } from "react";
 import DataTable from "./ui/dataTable";
+import { cookies } from "next/headers";
 
 export default async function Page(props: {
-  searchParams?: Promise<{ query?: string; page?: string }>;
+  // searchParams?: Promise<{ query?: string; page?: string }>;
+  searchParams?: { query?: string; page?: string };
 }) {
-  const searchParams = await props.searchParams;
-  const query = searchParams?.query || "";
-  const currentPage = Number(searchParams?.page) || 0;
+  const cookieStore = await cookies();
+  const token = cookieStore.get("x-access-token")?.value;
 
-  const { result, page, totalPage } = await getInquiryList(currentPage, 10);
+  // const searchParams = await props.searchParams;
+  // const query = searchParams?.query || "";
+  // const currentPage = Number(searchParams?.page) || 0;
+
+  // const { result, page, totalPage } = await getInquiryList(currentPage, 10);
 
   return (
     <main className="w-full flex flex-col gap-y-[24px]">
@@ -21,9 +26,10 @@ export default async function Page(props: {
 
       <div className="flex flex-col items-center gap-y-[60px] lg:gap-y-[80px]">
         <SearchBar placeholder="문의 제목 및 내용을 입력해주세요" />
+        <div>{token}</div>
 
         <Suspense fallback={<Loading />}>
-          <DataTable data={result} query={query} currentPage={currentPage} />
+          {/* <DataTable data={result} query={query} currentPage={currentPage} /> */}
           {/* <DataTable
             data={data.result}
             query={query}
@@ -31,7 +37,7 @@ export default async function Page(props: {
           /> */}
         </Suspense>
 
-        <Pagination totalPage={totalPage} currentPage={page} />
+        {/* <Pagination totalPage={totalPage} currentPage={page} /> */}
         {/* <Pagination totalPage={data.totalPage} currentPage={data.page} /> */}
       </div>
     </main>
--- src/features/inquiry/inquiry.ts
+++ src/features/inquiry/inquiry.ts
@@ -18,6 +18,7 @@ export async function getInquiryList(
     method: "GET",
     headers: {
       "Content-Type": "application/json",
+
       cookie,
     },
     credentials: "include",
