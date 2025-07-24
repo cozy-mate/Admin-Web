54783c2cc2da7321d73f9a67b593737e8373d11d
JJHyeok13 <jjinh99@naver.com>
2025-07-24 17:04:24 +0900

    fix: API 요청 헤더에 쿠키 명시적으로 추가

--- src/features/inquiry/inquiry.ts
+++ src/features/inquiry/inquiry.ts
@@ -1,5 +1,6 @@
 import { InquiryAnswer, InquiryItem } from "@/shared/types/inquiry";
 import { Pagination } from "@/shared/types/page";
+import { headers } from "next/headers";
 
 // 코지메이트 전체 문의 리스트 조회
 export async function getInquiryList(
@@ -10,16 +11,20 @@ export async function getInquiryList(
   url.searchParams.set("page", String(page));
   url.searchParams.set("size", String(size));
 
+  const reqHeaders = await headers();
+  const cookie = reqHeaders.get("cookie") ?? "";
+
   const res = await fetch(url.toString(), {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
+      cookie,
     },
     credentials: "include",
   });
 
   if (!res.ok) {
-    throw new Error(`문의 리스트 조회 실패 ${url}`);
+    throw new Error(`문의 리스트 조회 실패 ${cookie}`);
   }
 
   return res.json();
@@ -29,19 +34,23 @@ export async function getInquiryList(
 export async function getInquiryDetail(
   inquiryId: number
 ): Promise<InquiryItem> {
+  const reqHeaders = await headers();
+  const cookie = reqHeaders.get("cookie") ?? "";
+
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/admin/inquiries/${inquiryId}`,
     {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
+        cookie,
       },
       credentials: "include",
     }
   );
 
   if (!res.ok) {
-    throw new Error("문의 상세 조회 실패");
+    throw new Error(`문의 상세 조회 실패 ${cookie}`);
   }
 
   return res.json();
